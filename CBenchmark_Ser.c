97d0ad885205517ac3eae7d2b02c6c563154318e
Mohammed Al-Harazi <55173638+malharazi34@users.noreply.github.com>
2021-04-02 14:32:21 -0400

    Update CBenchmark_Ser.c

--- CBenchmark_Ser.c
+++ CBenchmark_Ser.c
@@ -1,3 +1,4 @@
+//Function Headers
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,13 +6,21 @@
 #include "timer.h"
 
 
-
+/*
+ * Function: rand_mult
+ * ---------------------------
+ * Populates two arrays with random numbers, the performs cross multiplication between the two arrys
+ *      and stores them in a third array. 
+ * 
+ *  n: size of the arrays, which will then run through a for loop n*n times
+ */
 void rand_multiplication(int n) {
 
     struct timeval start, end;
     int A[n], B[n], C[n], i;
     memset(C, 0, n);
 
+    //Populate the two arrays with numbers between 0 and 100
     for(int i = 0; i < n; i++) {
 
         A[i] = i;
@@ -22,14 +31,15 @@ void rand_multiplication(int n) {
         B[i] = i;
     }
 
-    START_TIMER(MatrixMult);
+    //Performs a cross multiplication between the two arrays and stores them in array C while recording the runtime
+    START_TIMER(RandMult);
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++)
             C[i] = A[i] * B[j];
     }
 
-   STOP_TIMER(MatrixMult);
+   STOP_TIMER(RandMult);
 
     printf("Work took %8.4fs seconds\n", GET_TIMER(MatrixMult));
     
@@ -39,8 +49,9 @@ int main(int argc, char *argv[]) {
 
     int numT, numA;
 
+    //Error checking to make sure the number of arguments in the command line are correct
     if (argc != 2) {
-        fprintf(stderr, "Invalid format: OMP_NUM_THREADS= <Number of Arguments> \n");
+        fprintf(stderr, "Invalid format: srun ./CBenchmark_Ser <Number of Arguments> \n");
         return EXIT_FAILURE;
     }
 
