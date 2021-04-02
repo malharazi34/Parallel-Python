fe95d6424ecc4ebe51069e679dbd5ea89758226b
Mohammed Al-Harazi <55173638+malharazi34@users.noreply.github.com>
2021-04-02 14:44:30 -0400

    Update PyBenchmark.py

--- PyBenchmark.py
+++ PyBenchmark.py
@@ -1,9 +1,20 @@
+# Import require modules
 import time
 import random
 import sys
 from multiprocessing import Pool
 
 
+
+#
+# Function: rand_mult
+# ---------------------------
+# Performs the inside loop of the C cross multiplication nested loop with a processing pool to
+#   divide the data among processes
+# 
+#  A: array of n random numbers
+#  B: array of n random numbers
+#  C: Holds the current loops multiplication results
 def rand_mult(A, B, C, i):
     
     for j in range(len(B)):
@@ -15,30 +26,34 @@ def rand_mult(A, B, C, i):
 
 if __name__ == "__main__":
     
-    
+    # Error Checking to make sure the correct number of arguments were given
     if len(sys.argv) < 2:
-        print("Invalid format: python3 rand_thread <Thread number> <Number of args>")
+        print("Invalid format: python3 PyBenchmark <Thread number> <Number of args>")
         sys.exit(1)
     
+    #Initialize the arguments for later use
     numP = int(sys.argv[1])
     numA = int(sys.argv[2])
 
+    #Populate arrays with random numbers
     A = [random.randint(0,100) for i in range(numA)]
     B = [random.randint(0,100) for i in range(numA)]
     C = [0 for i in range(numA)]
 
 
     
-    starttime = time.time()
+    START_TIMER(RandMult)
+    # Create a pool of processes based on the input given
     pool = Pool(processes = numP)
     
-    
+    #Divide the data in the first for loop among the processes
     for i in range(numA):
         pool.apply_async(rand_mult, args = (A,B,C,i))
     
+    #Cleanup the processes
     pool.close()
     pool.join()
-    endtime = time.time()
+    STOP_TIMER(RandMult)
     
 
-    print(f"Time taken {endtime-starttime} seconds")
+    print(f"Time taken {GET_TIMER(RandMult)} seconds")
