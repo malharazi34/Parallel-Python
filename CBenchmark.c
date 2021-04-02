00da5715db123021e2e5d1081c5d8b0a844594bf
Mohammed Al-Harazi <55173638+malharazi34@users.noreply.github.com>
2021-04-02 14:29:05 -0400

    Update CBenchmark.c

--- CBenchmark.c
+++ CBenchmark.c
@@ -1,35 +1,45 @@
+//Function Headers
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <omp.h>
 #include "timer.h"
 
-
+/*
+ * Function: rand_mult
+ * ---------------------------
+ * Populates two arrays with random numbers, the performs cross multiplication between the two arrys
+ *      and stores them in a third array. 
+ * 
+ *  n: size of the arrays, which will then run through a for loop n*n times
+ */
 
 void rand_multiplication(int n) {
 
     int A[n], B[n], C[n];
     memset(C, 0, n);
-
+    
+    //Populate the two arrays with numbers between 0 and 100
     for(int i = 0; i < n; i++) {
 
-        A[i] = i;
+        A[i] = (rand() % (100 - 0 + 1)) + 0;
     }
 
     for(int i = 0; i < n; i++) {
 
-        B[i] = i;
+        B[i] = (rand() % (100 - 0 + 1)) + 0;;
     }
 
-    START_TIMER(MatrixMult);
+    //Performs a cross multiplication between the two arrays using OpenMP and records the run time
+    START_TIMER(RandMult);
 #   pragma omp parallel for collapse(2)
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++)
             C[i] = A[i] * B[j];
     }
 
-    STOP_TIMER(MatrixMult);
-    printf("Work took %8.4fs seconds\n", GET_TIMER(MatrixMult));
+    STOP_TIMER(RandMult);
+    printf("Work took %8.4fs seconds\n", GET_TIMER(RandMult));
     
 }
 
@@ -37,8 +47,9 @@ int main(int argc, char *argv[]) {
 
     int numA;
 
+    //Error checking to make sure the number of arguments in the command line are correct
     if (argc != 2) {
-        fprintf(stderr, "Invalid format: OMP_NUM_THREADS= <Number of Arguments> \n");
+        fprintf(stderr, "Invalid format: srun OMP_NUM_THREADS=<NUM_PROCESSES> ./CBenchmark <Number of Arguments> \n");
         return EXIT_FAILURE;
     }
 
