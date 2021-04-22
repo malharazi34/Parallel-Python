0b621c6f06c57ffad7611697866d62b2c9965541
Mohammed Al-Harazi <55173638+malharazi34@users.noreply.github.com>
2021-04-22 18:58:36 -0400

    Update PyBenchmark_Ser.py

--- PyBenchmark_Ser.py
+++ PyBenchmark_Ser.py
@@ -1,49 +1,39 @@
-# Import require modules
-
 import time
 import random
 import sys
 
 
-#
-# Function: rand_mult
-# ---------------------------
-# Performs the inside loop of the C cross multiplication nested loop with a processing pool to
-#   divide the data among processes
-# 
-#  A: array of n random numbers
-#  B: array of n random numbers
-#  C: Holds the current loops multiplication results
-#  n: number of arguments to loop over n*n times
-def rand_mult(A, B, C, n):
+def rand_mult(x):
     
-    for i in range(n):
-        for j in range(n):
-            C[i] = A[i] * B[j]
-
-    
-    
+    b = x
+    for i in range(0, 100):
+        for j in range(0, 100):
+            b = b * i * j
+    return b
     
     
 
 
 if __name__ == "__main__":
-
-    #Error handling of command line arguments
-    if len(sys.argv) < 2:
-        print("Invalid format: python3 rand_thread <Number of args>")
+    
+    
+    if len(sys.argv) < 1:
+        print("Invalid format: python3 rand_thread <Thread number> <Number of args>")
         sys.exit(1)
-
     
     numA = int(sys.argv[1])
 
-    #Populate the arrays with n random numbers between 0 and 100
-    A = [random.randint(0,100) for i in range(numA)]
-    B = [random.randint(0,100) for i in range(numA)]
-    C = [0 for i in range(numA)]
+
+    C = [random.randint(0,100) for i in range(numA)]
+
+    starttime = time.time()
+    for i in range(len(C)):
+        C[i] = rand_mult(C[i])
+
 
     
-    start = time.time()
-    rand_mult(A, B, C, numA)
-    end - time.time()
-    print(f"Time taken {end-start} seconds")
+    
+    endtime = time.time()
+    
+
+    print(f"Time taken {endtime-starttime} seconds")
