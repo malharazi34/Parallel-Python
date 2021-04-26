fe0693977c478fe143b8e2a1a76e38be37fc13d5
Mohammed Al-Harazi <55173638+malharazi34@users.noreply.github.com>
2021-04-26 01:02:07 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -6,7 +6,7 @@
 
 make
 
-for n in 2500 5000 10000 15000 20000 25000 40000; do
+for n in 5000 10000 20000 25000 40000 50000 75000 85000 100000 150000 200000; do
 
     echo -n "n=$n c serial "
     ./CBenchmark_Ser $n
@@ -16,6 +16,8 @@ for n in 2500 5000 10000 15000 20000 25000 40000; do
         OMP_NUM_THREADS="$t" ./CBenchmark $n
     done
 
+    echo "-----------------------------------------------------------"
+
     echo -n "n=$n py serial "
     python3 PyBenchmark_Ser.py $n
 
@@ -24,6 +26,7 @@ for n in 2500 5000 10000 15000 20000 25000 40000; do
         python3 PyBenchmark.py $t $n
     done
 
+    echo "-----------------------------------------------------------"
     echo -n "n=$n pypy serial "
     ./pypy3.6-v7.3.3-linux64/bin/pypy3 PyBenchmark_Ser.py $n
 
@@ -32,5 +35,15 @@ for n in 2500 5000 10000 15000 20000 25000 40000; do
         ./pypy3.6-v7.3.3-linux64/bin/pypy3 PyBenchmark.py $t $n
     done
 
+    echo "-----------------------------------------------------------"
+    echo -n "n=$n cython serial"
+    python Cython-0.28.1/Cy_ser_test.py $n
+
+    for t in 1 2 4 8 16; do
+        echo -n "n=$n t=$t cython "
+        python Cython-0.28.1/Cy_multi_test.py $t $n
+    done
+
     echo ""
 done
+
