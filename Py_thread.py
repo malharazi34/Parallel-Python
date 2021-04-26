4e2ede65dd497c4f05b7e27db3fe9781b14f9489
Mohammed Al-Harazi <55173638+malharazi34@users.noreply.github.com>
2021-04-26 16:39:48 -0400

    Update Py_thread.py

--- Py_thread.py
+++ Py_thread.py
@@ -5,9 +5,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 
-
-
-def rand_mult(x):
+def rand_mult(x, C, numA, numT, ):
     
    
     beg = int(numA/numT) * x
@@ -24,29 +22,28 @@ def rand_mult(x):
                 
     
     
+
+def main():
+    if len(sys.argv) < 1:
+        print("Invalid format: python3 rand_thread <Thread number> <Number of args>")
+        sys.exit(1)
+    
+    numT = int(sys.argv[1])
+    numA = int(sys.argv[2])
+
+
+    C = [random.randint(0,100) for i in range(numA)]
+    
+    starttime = time.time()
+
+    with ThreadPoolExecutor(max_workers=numT) as executor:
+        for i in range(numT):
+            executor.submit(rand_mult, i, C, numA, numT)
+
+    endtime = time.time()
     
 
+    print(f"Time taken {endtime-starttime} seconds")
 
-    
-if len(sys.argv) < 1:
-    print("Invalid format: python3 rand_thread <Thread number> <Number of args>")
-    sys.exit(1)
-    
-numT = int(sys.argv[1])
-numA = int(sys.argv[2])
-
-
-C = [random.randint(0,100) for i in range(numA)]
-    
-thread_list = []
-    
-starttime = time.time()
-
-with ThreadPoolExecutor(max_workers=numT) as executor:
-    for i in range(numT):
-        executor.submit(rand_mult, i)
-
-endtime = time.time()
-    
-
-print(f"Time taken {endtime-starttime} seconds")
+if __name__ == "__main__":
+    main()
