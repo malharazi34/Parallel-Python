25e642c70b6308f8bf96d9f34c3bf75ef9aead51
Mohammed Al-Harazi <55173638+malharazi34@users.noreply.github.com>
2021-04-29 17:57:38 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -12,7 +12,7 @@ for n in 5000 10000 20000 25000 40000 50000 75000 85000 100000 150000 200000; do
     ./CBenchmark_Ser $n
 
     for t in 1 2 4 8 16; do
-        echo -n "n=$n t=$t C W/ Processes: "
+        echo -n "n=$n t=$t C W/ Threads: "
         OMP_NUM_THREADS="$t" ./CBenchmark $n
     done
 
@@ -21,27 +21,27 @@ for n in 5000 10000 20000 25000 40000 50000 75000 85000 100000 150000 200000; do
     echo -n "n=$n Python Serial: "
     python3 PyBenchmark_Ser.py $n
 
-    for t in 1 2 4 8 16; do
-        echo -n "n=$n t=$t Python W/ Processes: "
-        python3 PyBenchmark.py $t $n
+    for p in 1 2 4 8 16; do
+        echo -n "n=$n p=$t Python W/ Processes: "
+        python3 PyBenchmark.py $p $n
     done
 
     echo "-----------------------------------------------------------"
     echo -n "n=$n Pypy Serial: "
     ./pypy3.6-v7.3.3-linux64/bin/pypy3 PyBenchmark_Ser.py $n
 
-    for t in 1 2 4 8 16; do
-        echo -n "n=$n t=$t Pypy W/ Processes: "
-        ./pypy3.6-v7.3.3-linux64/bin/pypy3 PyBenchmark.py $t $n
+    for p in 1 2 4 8 16; do
+        echo -n "n=$n p=$p Pypy W/ Processes: "
+        ./pypy3.6-v7.3.3-linux64/bin/pypy3 PyBenchmark.py $p $n
     done
 
     echo "-----------------------------------------------------------"
     echo -n "n=$n Cython Serial"
     python Cython-0.28.1/Cy_ser_test.py $n
 
-    for t in 1 2 4 8 16; do
-        echo -n "n=$n t=$t Cython W/ Processes: "
-        python Cython-0.28.1/Cy_multi_test.py $t $n
+    for p in 1 2 4 8 16; do
+        echo -n "n=$n p=$p Cython W/ Processes: "
+        python Cython-0.28.1/Cy_multi_test.py $p $n
     done
     
     
